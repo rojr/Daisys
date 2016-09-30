d7f96ce75f9e3b98741a2ca5bd7d137c2e75fd0b
robmadeyou <robmadeyou@gmail.com>
2016-09-30 22:43:10 +0100

    Boom, superCMS is now a submodule

---
+++ settings/app.config.php
@@ -0,0 +1,3 @@
+<?php
+
+$application = new \Daisys\DaisysApplication();
---
+++ src/DaisysApplication.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Daisys;
+
+use Rhubarb\Crown\Application;
+use Rhubarb\Crown\Encryption\HashProvider;
+use Rhubarb\Crown\Encryption\Sha512HashProvider;
+use Rhubarb\Crown\Layout\LayoutModule;
+use Rhubarb\Leaf\LeafModule;
+use Rhubarb\Stem\Custard\SeedDemoDataCommand;
+use Rhubarb\Stem\Repositories\MySql\MySql;
+use Rhubarb\Stem\Repositories\Repository;
+use Rhubarb\Stem\StemModule;
+use SuperCMS\Custard\ApplicationDemoDataSeeder;
+use SuperCMS\Layouts\DefaultLayout;
+use SuperCMS\SuperCMS;
+
+class DaisysApplication extends Application
+{
+    protected function initialise()
+    {
+        parent::initialise();
+
+        if (file_exists(APPLICATION_ROOT_DIR . "/settings/site.config.php")) {
+            include_once(APPLICATION_ROOT_DIR . "/settings/site.config.php");
+        }
+
+        $this->developerMode = true;
+
+        Repository::setDefaultRepositoryClassName(MySql::class);
+
+        //SolutionSchema::registerSchema('HackTheHubSchema', HackTheHubSolutionSchema::class);
+
+        HashProvider::setProviderClassName(Sha512HashProvider::class);
+    }
+
+    protected function getModules()
+    {
+        return [
+            new SuperCMS($this->container()),
+            new LayoutModule(DefaultLayout::class),
+            new StemModule(),
+            new LeafModule(),
+        ];
+    }
+
+    protected function registerUrlHandlers()
+    {
+        parent::registerUrlHandlers();
+    }
+
+    public function getCustardCommands()
+    {
+        SeedDemoDataCommand::registerDemoDataSeeder(new ApplicationDemoDataSeeder());
+
+        return parent::getCustardCommands();
+    }
+}
